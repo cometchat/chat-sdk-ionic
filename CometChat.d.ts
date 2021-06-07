825a137ad49ca6772557f09ea709013625ea6645
Mayur Bhandari <mayur.bhandari@inscripts.com>
2021-06-07 21:43:11 +0530

    v2.3.4

--- CometChat.d.ts
+++ CometChat.d.ts
@@ -926,6 +926,7 @@ export namespace CometChat {
         protected deletedAt: number;
         protected deletedBy: string;
         protected replyCount: number;
+        protected rawMessage: Object;
         constructor(receiverId: string, messageType: string, receiverType: string, category: string);
         getId(): number;
         setId(value: number): void;
@@ -970,6 +971,8 @@ export namespace CometChat {
         getDeletedBy(): string;
         setReplyCount(replyCount: number): void;
         getReplyCount(): number;
+        setRawMessage(rawMessage: Object): void;
+        getRawMessage(): Object;
     }
 
     export class TextMessage extends BaseMessage implements Message {
